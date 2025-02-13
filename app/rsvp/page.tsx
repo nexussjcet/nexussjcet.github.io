e2be89540d0c300a6b2e00c8758ffb5a09e1b1a8
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2025-02-13 10:28:34 +0530

    feat: RSVP form

---
+++ app/rsvp/page.tsx
@@ -0,0 +1,10 @@
+import { Formbricks } from "@/components/formbrick";
+
+export default function Page() {
+	return (
+		<Formbricks
+			title="Mmede Techy Pidiya - RSVP"
+			src="https://app.formbricks.com/s/cm72uqxy5000sld0368600lj1"
+		/>
+	);
+}
