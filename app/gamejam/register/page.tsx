e20a2541b67d88f29dc41aeecf45053f7ff76250
Milansuman <milansuman2016@gmail.com>
2025-02-20 22:12:28 +0530

    add registration link for gamejam

---
+++ app/gamejam/register/page.tsx
@@ -0,0 +1,9 @@
+export default function Register(){
+  return <div className="relative h-screen overflow-auto">
+      <iframe 
+          src="https://app.formbricks.com/s/cm7djgf9f0000ih03tru4zjvr" 
+          frameBorder="0" 
+          className="absolute top-0 left-0 h-full w-full border-0 bg-black">
+      </iframe>
+  </div>
+}
