2e75d415be170c1507a4d3d6668be24d32a39205
ajayvarma007789 <vaajay322@gmail.com>
2025-03-25 09:47:57 +0530

    add techipedia form

---
+++ app/techipedia/page.tsx
@@ -0,0 +1,9 @@
+export default function Register(){
+    return <div className="relative h-screen overflow-auto">
+        <iframe 
+            src="https://app.formbricks.com/s/cm8nz0q9r0000l503lmi7eg4w" 
+            frameBorder="0" 
+            className="absolute top-0 left-0 h-full w-full border-0 bg-black">
+        </iframe>
+    </div>
+}
