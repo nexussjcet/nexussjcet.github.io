e862a58d6b3ad296ec8a8a0d74f7fcfe6d7c5bc7
Milansuman <milansuman2016@gmail.com>
2025-05-01 12:59:54 +0530

    add internship form

---
+++ app/internship/page.tsx
@@ -0,0 +1,11 @@
+export default function Internship() {
+  return (
+    <div className="relative h-screen overflow-auto">
+      <iframe 
+          src="https://app.formbricks.com/s/jfu2xx92t51h3slkpggntlse" 
+          frameBorder="0" 
+          className="absolute top-0 left-0 h-full w-full border-0 bg-black">
+      </iframe>
+  </div>
+  )
+}
