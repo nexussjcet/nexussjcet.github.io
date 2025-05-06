371b9929bb36ccdb138d91d885788fccb2e6e60c
Milansuman <milansuman2016@gmail.com>
2025-05-06 17:27:46 +0530

    change internship form

--- app/internship/page.tsx
+++ app/internship/page.tsx
@@ -2,7 +2,7 @@ export default function Internship() {
   return (
     <div className="relative h-screen overflow-auto">
       <iframe 
-          src="https://app.formbricks.com/s/jfu2xx92t51h3slkpggntlse" 
+          src="https://tally.so/r/mJg6pY" 
           frameBorder="0" 
           className="absolute top-0 left-0 h-full w-full border-0 bg-black">
       </iframe>
