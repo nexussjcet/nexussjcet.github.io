2ecc41b061c7fa8cdc73192db17d27a563d7df16
Milansuman <milansuman2016@gmail.com>
2025-07-29 21:56:03 +0530

    update techipedia

--- app/techipedia/page.tsx
+++ app/techipedia/page.tsx
@@ -1,7 +1,7 @@
 export default function Register(){
     return <div className="relative h-screen overflow-auto">
         <iframe 
-            src="https://app.formbricks.com/s/cm8nz0q9r0000l503lmi7eg4w" 
+            src="https://tally.so/r/3q1NEg" 
             frameBorder="0" 
             className="absolute top-0 left-0 h-full w-full border-0 bg-black">
         </iframe>
