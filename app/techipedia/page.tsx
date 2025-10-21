bffc25db3528b1a35e9a7a8db3fe7d24fc78827a
ShaunV334 <shaunvarghese43@gmail.com>
2025-10-22 00:46:19 +0530

    updated tally link

--- app/techipedia/page.tsx
+++ app/techipedia/page.tsx
@@ -1,7 +1,7 @@
 export default function Register() {
   return <div className="relative h-screen overflow-auto">
     <iframe
-      src="https://tally.so/r/3q29Yg"
+      src="https://tally.so/r/w4gZQY"
       frameBorder="0"
       className="absolute top-0 left-0 h-full w-full border-0 bg-black">
     </iframe>
