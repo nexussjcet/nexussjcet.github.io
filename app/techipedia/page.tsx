d348286ec97549b4d391633679665c544d784c53
Milansuman <milansuman2016@gmail.com>
2025-10-21 23:15:39 +0530

    change techypedia link

--- app/techipedia/page.tsx
+++ app/techipedia/page.tsx
@@ -1,9 +1,9 @@
-export default function Register(){
-    return <div className="relative h-screen overflow-auto">
-        <iframe 
-            src="https://tally.so/r/3q1NEg" 
-            frameBorder="0" 
-            className="absolute top-0 left-0 h-full w-full border-0 bg-black">
-        </iframe>
-    </div>
+export default function Register() {
+  return <div className="relative h-screen overflow-auto">
+    <iframe
+      src="https://tally.so/r/3q29Yg"
+      frameBorder="0"
+      className="absolute top-0 left-0 h-full w-full border-0 bg-black">
+    </iframe>
+  </div>
 }
